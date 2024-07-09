e87c09fcdb2466d9512a19c500a02fc927bf437b
shandriy <entyspite@gmail.com>
2024-07-09 11:19:40 -0700

    Reformat resize function code

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 addEventListener("DOMContentLoaded", function() {
   var canvas = document.getElementsByTagName("canvas")[0];
   var context = canvas.getContext("2d", { alpha: false });
+  canvas.width = 800;
+  canvas.height = 600;
   function resize() {
-    var outWidth = innerWidth, outHeight = innerHeight;
-    var top = 0, left = 0;
+    var outWidth, outHeight, top, left;
     if (innerWidth * 3 > innerHeight * 4) {
       outWidth = innerHeight * (4 / 3);
+      outHeight = innerHeight;
       left = (innerWidth - outWidth) / 2;
+      top = 0;
     } else {
       outHeight = innerWidth / (4 / 3);
+      outWidth = innerWidth;
       top = (innerHeight - outHeight) / 2;
+      left = 0;
     };
     canvas.style.top = top + "px";
     canvas.style.left = left + "px";
