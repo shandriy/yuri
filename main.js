04401321f36258a29890e6c453ba4a8ae3d4e5cb
shandriy <entyspite@gmail.com>
2024-07-10 10:11:40 -0700

    Basic collision

--- main.js
+++ main.js
@@ -83,13 +83,25 @@ addEventListener("DOMContentLoaded", function() {
   }
   var animate = window.requestAnimationFrame || setTimeout;
   var previous = Date.now(), now, delta;
+  var colliders = [
+    [[100, 200], [200, 200]],
+    [[200, 200], [200, 300]]
+  ];
   function frame() {
     now = Date.now()
     delta = now - previous;
     previous = now;
     context.fillStyle = "#fff";
+    context.lineWidth = 1;
+    context.strokeStyle = "#0f0";
     context.fillRect(0, 0, 800, 600);
-    context.fillStyle = "#f00";
+    context.beginPath();
+    var length = colliders.length;
+    for (var i = 0; i < length; i += 1) {
+      context.moveTo(colliders[i][0][0], colliders[i][0][1]);
+      context.lineTo(colliders[i][1][0], colliders[i][1][1]);
+    };
+    context.stroke();
     var xMovement = 0, yMovement = 0;
     var speed = keyDown("ShiftLeft") ? 0.5 : 0.2;
     if (keyDown("KeyW") || keyDown("ArrowUp")) yMovement -= 1;
@@ -100,9 +112,36 @@ addEventListener("DOMContentLoaded", function() {
       xMovement *= Math.SQRT1_2;
       yMovement *= Math.SQRT1_2;
     };
-    mc.x += xMovement * delta * speed;
-    mc.y += yMovement * delta * speed;
-    context.fillRect(Math.round(mc.x), Math.round(mc.y), 40, 80);
+    xMovement *= delta * speed;
+    yMovement *= delta * speed;
+    mc.x += xMovement;
+    mc.y += yMovement;
+    var xCalc = Math.round(mc.x);
+    var yCalc = Math.round(mc.y);
+    for (var i = 0; i < length; i += 1) {
+      var horizontal = colliders[i][0][1] - colliders[i][1][1] === 0;
+      if (horizontal && yMovement !== 0) {
+        var less = colliders[i][0][0] < colliders[i][1][0] ? colliders[i][0][0] : colliders[i][1][0];
+        var more = colliders[i][0][0] > colliders[i][1][0] ? colliders[i][0][0] : colliders[i][1][0];
+        if (xCalc + 15 > less && xCalc - 15 < more && yCalc >= colliders[i][0][1] && yCalc - 15 <= colliders[i][0][1]) {
+          while (yCalc >= colliders[i][0][1] && yCalc - 15 <= colliders[i][0][1])
+            yCalc += yMovement < 0 ? 1 : -1;
+          mc.y = yCalc;
+        }
+      } else if (!horizontal && xMovement !== 0) {
+        var less = colliders[i][0][1] < colliders[i][1][1] ? colliders[i][0][1] : colliders[i][1][1];
+        var more = colliders[i][0][1] > colliders[i][1][1] ? colliders[i][0][1] : colliders[i][1][1];
+        if (yCalc > less && yCalc - 15 < more && xCalc + 15 >= colliders[i][0][0] && xCalc - 15 <= colliders[i][0][0]) {
+          while (xCalc + 15 >= colliders[i][0][0] && xCalc - 15 <= colliders[i][0][0])
+            xCalc += xMovement < 0 ? 1 : -1;
+          mc.x = xCalc;
+        }
+      }
+    };
+    context.fillStyle = "#f00";
+    context.fillRect(Math.round(mc.x) - 20, Math.round(mc.y) - 80, 40, 80);
+    context.fillStyle = "#00f";
+    context.fillRect(Math.round(mc.x) - 15, Math.round(mc.y) - 15, 30, 15);
     animate(frame);
   };
 });
