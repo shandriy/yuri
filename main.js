d6a23665b460edaf344efe9242b17185a2bd3693
shandriy <entyspite@gmail.com>
2024-07-12 14:34:52 -0700

    Added slight acceleration

--- main.js
+++ main.js
@@ -135,7 +135,9 @@ addEventListener("DOMContentLoaded", function() {
     };
     context.stroke();
     var xMovement = 0, yMovement = 0;
-    var speed = keyDown("ShiftLeft") ? 0.5 : 0.25;
+    var speed = 0.25;
+    if (mc.walkingFor < 100)
+      speed *= (mc.walkingFor / 125) + 0.2;
     if (keyDown("KeyW") || keyDown("ArrowUp")) yMovement -= 1;
     if (keyDown("KeyS") || keyDown("ArrowDown")) yMovement += 1;
     if (keyDown("KeyA") || keyDown("ArrowLeft")) xMovement -= 1;
