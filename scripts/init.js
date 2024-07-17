ebb1945d1a0c92d2b588a81bb4abbbb3d9e0b646
shandriy <entyspite@gmail.com>
2024-07-16 19:48:38 -0700

    Added multiple scripts to loading process

--- scripts/init.js
+++ scripts/init.js
@@ -3,12 +3,19 @@ addEventListener("DOMContentLoaded", function() {
   var mcSprites = new Image(), mc;
   mcSprites.addEventListener("load", function() {
     mc = new yuri.Entity("mc", true, mcSprites, 80, 160);
-    var script = document.createElement("script");
-    script.addEventListener("load", function() {
-      game();
-    });
-    script.setAttribute("src", "scripts/game.js");
-    document.getElementsByTagName("head")[0].appendChild(script);
+    var scripts = ["game", "stage", "scene"];
+    var length = scripts.length;
+    var loadCount = 0;
+    function countUp() {
+      loadCount += 1;
+      if (loadCount >= length) game();
+    };
+    for (var i = 0; i < length; i += 1) {
+      var script = document.createElement("script");
+      script.addEventListener("load", countUp);
+      script.setAttribute("src", "scripts/" + scripts[i] + ".js");
+      document.getElementsByTagName("head")[0].appendChild(script);
+    };
   });
   mcSprites.src = "assets/images/mc_walk.png";
 });
