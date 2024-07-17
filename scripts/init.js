324281a2cf85a66006f03942b53afed04c08c41c
shandriy <entyspite@gmail.com>
2024-07-17 10:33:10 -0700

    Load assets with utils from engine

--- scripts/init.js
+++ scripts/init.js
@@ -1,21 +1,14 @@
 addEventListener("DOMContentLoaded", function() {
   yuri.init(800, 600, false, "#000");
-  var mcSprites = new Image(), mc;
-  mcSprites.addEventListener("load", function() {
+  var mc;
+  var mcSprites = utils.load.image("assets/images/mc_walk.png", function() {
     mc = new yuri.Entity("mc", true, mcSprites, 80, 160);
     var scripts = ["game", "stage", "scene"];
     var length = scripts.length;
     var loadCount = 0;
-    function countUp() {
+    utils.load.scripts(scripts, "scripts/", ".js", function() {
       loadCount += 1;
       if (loadCount >= length) game();
-    };
-    for (var i = 0; i < length; i += 1) {
-      var script = document.createElement("script");
-      script.addEventListener("load", countUp);
-      script.setAttribute("src", "scripts/" + scripts[i] + ".js");
-      document.getElementsByTagName("head")[0].appendChild(script);
-    };
+    })
   });
-  mcSprites.src = "assets/images/mc_walk.png";
 });
