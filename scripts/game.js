7b1ba1386440921207c7fe5fd4f9984a58203a3d
shandriy <entyspite@gmail.com>
2024-07-16 19:44:22 -0700

    Reformatted for speed and efficiency

--- scripts/game.js
+++ scripts/game.js
@@ -8,7 +8,7 @@ function game() {
     frame: 0,
     direction: 0,
     standing: true,
-    stateFor: 0,
+    stateFor: 101,
     temp: {
       x: 0,
       y: 0
@@ -17,23 +17,21 @@ function game() {
   yuri.frame(function(delta) {
     var frame = 0;
     var speed = 0.25;
+    mcObject.temp.x = 0;
+    mcObject.temp.y = 0;
     mcObject.stateFor += delta;
     yuri.props.context.fillStyle = "#fff";
     yuri.props.context.fillRect(0, 0, 800, 600);
-    var keyUpPressed = yuri.keyboard.isDown("KeyW") || yuri.keyboard.isDown("ArrowUp");
-    var keyLeftPressed = yuri.keyboard.isDown("KeyA") || yuri.keyboard.isDown("ArrowLeft");
-    var keyDownPressed = yuri.keyboard.isDown("KeyS") || yuri.keyboard.isDown("ArrowDown");
-    var keyRightPressed = yuri.keyboard.isDown("KeyD") || yuri.keyboard.isDown("ArrowRight");
-    if (keyUpPressed) mcObject.temp.y -= delta;
-    if (keyLeftPressed) mcObject.temp.x -= delta;
-    if (keyDownPressed) mcObject.temp.y += delta;
-    if (keyRightPressed) mcObject.temp.x += delta;
-    if (mcObject.temp.x < 0) mcObject.direction = 3;
-    if (mcObject.temp.x > 0) mcObject.direction = 2;
-    if (mcObject.temp.y < 0) mcObject.direction = 1;
-    if (mcObject.temp.y > 0) mcObject.direction = 0;
+    if (yuri.keyboard.isDown("KeyW") || yuri.keyboard.isDown("ArrowUp")) mcObject.temp.y -= delta;
+    if (yuri.keyboard.isDown("KeyA") || yuri.keyboard.isDown("ArrowLeft")) mcObject.temp.x -= delta;
+    if (yuri.keyboard.isDown("KeyS") || yuri.keyboard.isDown("ArrowDown")) mcObject.temp.y += delta;
+    if (yuri.keyboard.isDown("KeyD") || yuri.keyboard.isDown("ArrowRight")) mcObject.temp.x += delta;
     if (mcObject.temp.x !== 0 && mcObject.temp.y !== 0) speed *= Math.SQRT1_2;
     if (mcObject.temp.x !== 0 || mcObject.temp.y !== 0) {
+      if (mcObject.temp.x < 0) mcObject.direction = 3;
+      else if (mcObject.temp.x > 0) mcObject.direction = 2;
+      if (mcObject.temp.y < 0) mcObject.direction = 1;
+      else if (mcObject.temp.y > 0) mcObject.direction = 0;
       if (mcObject.standing) mcObject.stateFor = mcObject.stateFor > 100 ? 51 : 0;
       else if (mcObject.stateFor > 50) mcObject.frame += delta * 0.006;
       mcObject.standing = false;
@@ -45,10 +43,8 @@ function game() {
     };
     mcObject.x += mcObject.temp.x * speed;
     mcObject.y += mcObject.temp.y * speed;
-    mcObject.temp.x = 0;
-    mcObject.temp.y = 0;
-    mcEntity.x = Math.round(mcObject.x);
-    mcEntity.y = Math.round(mcObject.y);
+    mcEntity.x = ~~mcObject.x;
+    mcEntity.y = ~~mcObject.y;
     if (mcObject.frame > 0) frame = ~~((mcObject.frame % 4) + 1);
     mcEntity.animation = (mcObject.direction * 5) + frame;
   });
