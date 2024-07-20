dc5770026d9c0f8dd44a1f348812b833f4e963d9
shandriy <entyspite@gmail.com>
2024-07-20 11:56:17 -0700

    Draw text with fillText in newer versions

--- scripts/init.js
+++ scripts/init.js
@@ -1,15 +1,22 @@
 addEventListener("DOMContentLoaded", function() {
   yuri.init(800, 600, false, "#000");
   var mc;
+  var canvas = document.createElement("canvas");
+  var context = canvas.getContext("2d");
+  var fillText = !!context.fillText;
   var dialogue = new yuri.Entity("dialogue", false, function() {
-    var canvas = document.createElement("canvas");
-    var context = canvas.getContext("2d");
+    var lines = ["\"Hello World!\"", "... lines of text"];
+    var length = lines.length;
     yuri.props.context.fillStyle = "#000";
     yuri.props.context.fillRect(10, 360, 780, 230);
     yuri.props.context.lineWidth = 3;
     yuri.props.context.strokeStyle = "#fff";
     yuri.props.context.strokeRect(20, 370, 760, 210);
-    if (context.fillText) {
+    if (fillText) {
+      yuri.props.context.fillStyle = "#fff";
+      yuri.props.context.font = "25px 'Times New Roman'";
+      for (var i = 0; i < length; i += 1)
+        yuri.props.context.fillText(lines[i], 40, 407 + (i * 30));
     };
   });
   dialogue.z = Infinity;
